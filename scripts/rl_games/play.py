45d93295b90972453f685d3c25d8465bf743d30e
riemac <18817066197@163.com>
2026-03-10 15:01:37 +0800

    Fix rl_games play checkpoint import

--- scripts/rl_games/play.py
+++ scripts/rl_games/play.py
@@ -60,9 +60,8 @@ from rl_games.torch_runner import Runner
 from isaaclab.envs import DirectMARLEnv, multi_agent_to_single_agent
 from isaaclab.utils.assets import retrieve_file_path
 from isaaclab.utils.dict import print_dict
-from isaaclab.utils.pretrained_checkpoint import get_published_pretrained_checkpoint
-
 from isaaclab_rl.rl_games import RlGamesGpuEnv, RlGamesVecEnvWrapper
+from isaaclab_rl.utils.pretrained_checkpoint import get_published_pretrained_checkpoint
 
 import isaaclab_tasks  # noqa: F401
 from isaaclab_tasks.utils import get_checkpoint_path, load_cfg_from_registry, parse_env_cfg
